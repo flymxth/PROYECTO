3a46388464c281dec4adc5c9c89e09f669ef1398
flymxth <cugl070103hneszsa0@cbtis217.edu.mx>
2024-11-26 11:39:02 -0600

    Avance 9 parte 1

--- db.sql
+++ db.sql
@@ -22,8 +22,35 @@ CREATE TABLE sesion (
     FOREIGN KEY (nombreusuario) REFERENCES registro (nombreusuario)
 );
 
+DROP TABLE aportaciones;
+CREATE TABLE aportaciones (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR (50) NOT NULL,
+    apellidos VARCHAR (200) NOT NULL,
+    telefono VARCHAR (30) NOT NULL,
+    aportacion VARCHAR (60) NOT NULL,
+    fecha DATE NOT NULL
+);
+
+DROP TABLE citas;
+CREATE TABLE citas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    apellidos VARCHAR(100) NOT NULL,
+    telefono VARCHAR(15) NOT NULL,
+    sexo ENUM('Macho', 'Hembra') NOT NULL,
+    nombre_mascota VARCHAR(100) NOT NULL,
+    Raza VARCHAR(40) NOT NULL,
+    Edad VARCHAR (50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+SELECT * FROM citas;
+
 -- Inserccion de datos --
 
 INSERT INTO registro VALUES (NULL, 'Luis Antonio', 'Custodio Guzmán', 'flymxth', 'luis@gmail.com', '03-01-2007', 'Comprando2007');
 INSERT INTO registro VALUES (NULL, 'Jose Antonio', 'Duran Torres', 'Juancho0', 'jduran@gmail.com', '2007-01-03', 'Comprando2007');
 SELECT * FROM registro;
+
